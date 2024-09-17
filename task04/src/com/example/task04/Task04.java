09aa3369f281e388bd327836b1fc5ad1b14bd782
sdybybhon <artem563732@mail.ru>
2024-09-17 20:04:35 +0500

    Уфимцев Артём. Задание 04

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -3,19 +3,27 @@ package com.example.task04;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        switch (operation) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                if (b == 0) {
+                    System.out.println("Делать на 0 нельзя"); //обрабатываем исключение при делении на 0
+                    return 0;
+                } else {
+                    return (float) a / b;
+                }
+        }
         return 0;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        float result = calculate(-25, 5, "/");
+        float result = calculate(-25, 0, "/");
         System.out.println(result);
-        */
     }
 
 }
