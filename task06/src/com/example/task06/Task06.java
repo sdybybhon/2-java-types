b657765498492a7b37aee835babe398aa941d52a
sdybybhon <artem563732@mail.ru>
2024-09-17 20:07:22 +0500

    Уфимцев Артём. Задание 06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,17 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int sum = x + y;
 
-        return 0;
+        return String.valueOf(Math.abs(sum)).length();
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+
+        int result = solution(1212, 34125666);
         System.out.println(result);
-        */
     }
 
 }
